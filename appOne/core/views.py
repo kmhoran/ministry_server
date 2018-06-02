a4aa0c99e94aacd191ba617e423391049e46fb11
kmhoran <kevin.michael.horan@gmail.com>
2018-06-01 17:39:22 -0700

    play around with user login

---
+++ appOne/core/views.py
@@ -0,0 +1,14 @@
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+def register(request):
+    if request.method == 'post':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/')
--- ministry_server/settings.py
+++ ministry_server/settings.py
@@ -104,6 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+LOGIN_REDIRECT_URL = '/users/'
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
