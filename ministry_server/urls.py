32edbb04d518f74da7225ab503238212397f363b
kmhoran <kevin.michael.horan@gmail.com>
2018-06-02 19:26:25 -0700

    play around more with authentication and views

--- ministry_server/urls.py
+++ ministry_server/urls.py
@@ -3,13 +3,12 @@ from django.urls import include, path
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
 from appOne.core import views as core_views
+# from ministry_server import appOne
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', auth_views.login, name='login'),
-    path('logout/', auth_views.logout, {'next_page':'/outside'}, name='logout'),
-    path('register/', core_views.register, name='register'),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('users/', TemplateView.as_view(template_name='appOne/users.html')),
-    path('outside/', TemplateView.as_view(template_name='appOne/outside.html')),
+    path('members/', include('appOne.urls')),
     path('', TemplateView.as_view(template_name="appOne/index.html"))
 ]
